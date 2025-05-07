bd4f9427cc3475d267fa5a786ee6cb90ee5daba8
Sameer Gupta <Sameerg@emids.com>
2025-05-07 22:44:35 +0530

    Added the program for day 1

---
+++ Week1/Day1/BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator.")
+user_name = input("Please enter your name:\n")
+pet_name = input("Please enter the name of your pet: \n")
+print("The name of your band can be :", user_name, pet_name)
