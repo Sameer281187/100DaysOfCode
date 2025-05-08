6493594c78665964c7f2eb5070783ec8859986ca
Sameer Gupta <Sameerg@emids.com>
2025-05-08 09:44:03 +0530

    Adden comments to make the code understandable

--- Week1/Day1/BandNameGenerator.py
+++ Week1/Day1/BandNameGenerator.py
@@ -1,4 +1,9 @@
+# Give a salutation for the end user using this program
 print("Welcome to Band Name Generator.")
+
+# Save the user inputs in a variable to reference the input values later in the program
 user_name = input("Please enter your name:\n")
 pet_name = input("Please enter the name of your pet: \n")
+
+# Print the output using string concatenation
 print("The name of your band can be :", user_name, pet_name)
