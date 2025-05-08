0fe631148095b65a3f167e9ac12744076169ed4d
Sameer Gupta <Sameerg@emids.com>
2025-05-08 10:10:36 +0530

    Added new practice code for Day 2

---
+++ Week1/Day2/Typechecking.py
@@ -0,0 +1,13 @@
+# checking the different data types that are present in python
+
+# String data type
+print(type("abc"))
+
+# Integer data type
+print(type(123))
+
+# Float Data type
+print(type(123.45))
+
+# Boolean data type
+print(type(True))
