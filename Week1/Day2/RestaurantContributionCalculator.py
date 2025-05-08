c1947e5ddb3b8638919ad37e4c352ad78169d601
Sameer Gupta <Sameerg@emids.com>
2025-05-08 12:00:59 +0530

    minor formatting changes

--- Week1/Day2/RestaurantContributionCalculator.py
+++ Week1/Day2/RestaurantContributionCalculator.py
@@ -4,6 +4,6 @@ total_bill = float(input("What is your total bill amount ? INR "))
 tip_percentage = float(input("What %age of total bill do you want to give as a tip ? 5, 7 or 10 ? "))
 no_of_people = int(input("How many people to split the bill ? "))
 
-contribution_amount = round((total_bill * (1+(tip_percentage/100))) / no_of_people, 2)
+contribution_amount = round((total_bill * (1 + (tip_percentage / 100))) / no_of_people, 2)
 
 print(f"Each person should pay: {contribution_amount}")
