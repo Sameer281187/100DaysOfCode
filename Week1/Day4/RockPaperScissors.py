086c61b693329b0002f6b055913fd9f7e93e4c6b
Sameer Gupta <Sameerg@emids.com>
2025-05-10 18:53:20 +0530

    optimized code for rock paper scissors

--- Week1/Day4/RockPaperScissors.py
+++ Week1/Day4/RockPaperScissors.py
@@ -1,22 +1,15 @@
 import random
 from contextlib import nullcontext
-
-print("***********************Welcome to Rock Paper and Scissors game***********************")
-game_values = ["Rock", "Paper", "Scissors"]
-player_1_selection = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"))
-
-print("Player 1 chose: \n \n")
-if player_1_selection == 0:
-    print('''
+rock = '''
        ,--.--._
 ------" _, \___)
         / _/____)
         \//(____)
 ------\     (__)
        `-----"
-    ''')
-elif player_1_selection == 1:
-    print('''
+'''
+
+paper = '''
            ___..__
   __..--""" ._ __.'
               "-..__
@@ -24,9 +17,9 @@ elif player_1_selection == 1:
  ___        '--...__"";
     `-..__ '"---..._;"
           """"----'  
-    ''')
-elif player_1_selection == 2:
-    print('''
+'''
+
+scissors = '''
    ____
   / __ \
  ( (__) |___ ___
@@ -35,7 +28,18 @@ elif player_1_selection == 2:
   / __   __`.___-----""""
  ( (__) |
   \____/
-    ''')
+'''
+print("***********************Welcome to Rock Paper and Scissors game***********************")
+game_values = ["Rock", "Paper", "Scissors"]
+player_1_selection = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"))
+
+print("Player 1 chose: \n \n")
+if player_1_selection == 0:
+    print(rock)
+elif player_1_selection == 1:
+    print(paper)
+elif player_1_selection == 2:
+    print(scissors)
 else:
     print("You entered an invalid number. You lose")
     exit(1)
@@ -44,35 +48,11 @@ computer_selection = random.randint(0,2)
 
 print("Computer chose: \n \n")
 if computer_selection == 0:
-    print('''
-       ,--.--._
-------" _, \___)
-        / _/____)
-        \//(____)
-------\     (__)
-       `-----"
-    ''')
+    print(rock)
 elif computer_selection == 1:
-    print('''
-           ___..__
-  __..--""" ._ __.'
-              "-..__
-            '"--..__";
- ___        '--...__"";
-    `-..__ '"---..._;"
-          """"----'  
-    ''')
+    print(paper)
 else:
-    print('''
-   ____
-  / __ \
- ( (__) |___ ___
-  \________,'   """""----....____
-   _______<  () dd       ____----'
-  / __   __`.___-----""""
- ( (__) |
-  \____/
-    ''')
+    print(scissors)
 
 winner = ""
 
