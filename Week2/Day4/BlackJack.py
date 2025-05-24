08eef4ba2921dff019b86fcb00482bf1449813ee
Sameer Gupta <Sameerg@emids.com>
2025-05-24 12:48:14 +0530

    refactored code

--- Week2/Day4/BlackJack.py
+++ Week2/Day4/BlackJack.py
@@ -1,18 +1,19 @@
 import random
 import BlackJackArt
 
-def initial_draw(all_cards):
+def initial_draw(all_cards, cards):
     """ Draws the initial two card numbers for player and one card number for computer"""
     for i in range(2):
         if i < 1:
-            cards_drawn["player"].append(random.choice(all_cards))
-            cards_drawn["computer"].append(random.choice(all_cards))
+            cards["player"].append(random.choice(all_cards))
+            cards["computer"].append(random.choice(all_cards))
         else:
-            cards_drawn["player"].append(random.choice(all_cards))
+            cards["player"].append(random.choice(all_cards))
 
-def check_win_by_blackjack(score):
+def check_win_by_blackjack(score, cards):
     """ Checks if the player wins by blackjack after initial draw"""
     if score == 21:
+        display_initial_scores(score, cards)
         print("Won by Blackjack. You won")
         exit(1)
 
@@ -24,6 +25,8 @@ def display_initial_scores(player, cards):
 def want_to_play():
     """Checks with player if they want to continue playing"""
     is_play = input("Do you want to play a game of blackjack? Type 'y' or 'n': ")
+    print("\n" * 20)
+    display_graphics()
     continue_play = True if is_play == "y" else False
     return continue_play
 
@@ -37,34 +40,35 @@ def display_final_result(player, computer):
         print("You went over, You Lose")
     elif computer > 21:
         print("Opponent went over, You win")
-    elif player == computer_score:
+    elif player == computer:
         print("Draw")
-    elif player > computer_score:
+    elif player > computer:
         print("You Won")
     else:
         print("You Lose")
 
-all_card_numbers = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-display_graphics()
-is_continue = want_to_play()
-
-while is_continue:
+def play_game():
     cards_drawn = {
         "player": [],
         "computer": []
     }
-    initial_draw(all_card_numbers)
+    initial_draw(all_card_numbers, cards_drawn)
 
     player_score = sum(cards_drawn["player"])
     computer_score = sum(cards_drawn["computer"])
 
-    check_win_by_blackjack(player_score)
+    check_win_by_blackjack(player_score, cards_drawn)
     display_initial_scores(player_score, cards_drawn)
 
     while player_score <= 21:
-        is_continue = input("Type 'y' to get another card, type 'n' to pass: ")
-        if is_continue == "y":
-            cards_drawn["player"].append(random.choice(all_card_numbers))
+        continue_playing = input("Type 'y' to get another card, type 'n' to pass: ")
+        if continue_playing == "y":
+            latest_card = random.choice(all_card_numbers)
+            cards_drawn["player"].append(latest_card)
+            player_score = sum(cards_drawn["player"])
+            if player_score > 21 and latest_card == 11:
+                cards_drawn["player"].remove(latest_card)
+                cards_drawn["player"].append(1)
             player_score = sum(cards_drawn["player"])
             display_initial_scores(player_score, cards_drawn)
         else:
@@ -80,10 +84,12 @@ while is_continue:
 
     display_final_result(player_score, computer_score)
 
+all_card_numbers = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+is_continue = want_to_play()
+while is_continue:
+    play_game()
     is_continue = want_to_play()
-    print("\n" * 20)
-    if is_continue:
-        display_graphics()
 
 
 
